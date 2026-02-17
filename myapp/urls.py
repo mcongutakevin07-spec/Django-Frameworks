3eb32be7b84e34fb4f211e05d1d76d8bffc46b84
Owuondajoshua <joshua.owuonda@strathmore.edu>
2026-02-18 01:53:44 +0300

    updated todo app

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("todos/", views.todos, name="todos")
-   
+    path('', views.home, name='home'),
+    path('add/', views.add_todo, name='add_todo'),
+    path('update/<int:id>/', views.update_todo, name='update_todo'),
+    path('delete/<int:id>/', views.delete_todo, name='delete_todo'),
+    path('toggle/<int:id>/', views.toggle_complete, name='toggle_complete'),
+
+    # API
+    path('api/todos/', views.TodoListAPI.as_view(), name='api_todos'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from .models import TodoItem
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+@login_required
 def home(request):
-    return render(request,"home.html")
+    todos = TodoItem.objects.filter(user=request.user)
+    return render(request, "todos.html", {"todos": todos})
 
-def todos(request):
-    items = TodoItem.objects.all()
-    return render(request,"todos.html",{"todos":items})
- 
- 
+
+@login_required
+def add_todo(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+
+        TodoItem.objects.create(
+            user=request.user,
+            title=title,
+            description=description
+        )
+
+        return redirect("home")  # or redirect to todos page
+
+    return render(request, "add_todo.html")
+
+
+
+@login_required
+def update_todo(request, id):
+    todo = get_object_or_404(TodoItem, id=id, user=request.user)
+
+    todo.title = request.POST.get("title")
+    todo.description = request.POST.get("description")
+    todo.save()
+
+    return JsonResponse({"status": "updated"})
+
+
+@login_required
+def delete_todo(request, id):
+    todo = get_object_or_404(TodoItem, id=id, user=request.user)
+    todo.delete()
+    return JsonResponse({"status": "deleted"})
+
+
+@login_required
+def toggle_complete(request, id):
+    todo = get_object_or_404(TodoItem, id=id, user=request.user)
+    todo.completed = not todo.completed
+    todo.save()
+    return JsonResponse({"completed": todo.completed})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+# REST API
+class TodoListAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        todos = TodoItem.objects.filter(user=request.user)
+        data = [
+            {
+                "id": t.id,
+                "title": t.title,
+                "description": t.description,
+                "completed": t.completed,
+            }
+            for t in todos
+        ]
+        return Response(data)
+
 
 
 
